242b96b954ccfb67934463aa3f520281c1546f0a
Brian Antonio Garduño Martínez <briangar23@outlook.com>
2025-05-03 14:56:08 -0600

    Actualizo App.js

--- App.js
+++ App.js
@@ -1,16 +1,22 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import LoginScreen from "./screens/LoginScreen";
-import RegisterScreen from "./screens/RegisterScreen"; // Asegúrate de tener esta pantalla
+import RegisterScreen from "./screens/RegisterScreen"; // Asegúrate de tener esta pantalla creada
+import SplashScreen from "./screens/SplashScreen"; // Asegúrate de tener esta pantalla creada
+import LanguageSelectionScreen from "./screens/LanguageSelectionScreen";
+import SpecialtySelectionScreen from "./screens/SpecialtySelectionScreen";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator initialRouteName="Splash" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Splash" component={SplashScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="LanguageSelection" component={LanguageSelectionScreen} />
+        <Stack.Screen name="SpecialtySelection" component={SpecialtySelectionScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
