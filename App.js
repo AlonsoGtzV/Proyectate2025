f03623bf534fb9f79e125e31b955bf0fcb45ccb2
Brian Antonio Garduño Martínez <briangar23@outlook.com>
2025-05-05 15:22:21 -0600

    Actualizo App

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import RegisterScreen from "./screens/RegisterScreen"; // Asegúrate de tener es
 import SplashScreen from "./screens/SplashScreen"; // Asegúrate de tener esta pantalla creada
 import LanguageSelectionScreen from "./screens/LanguageSelectionScreen";
 import SpecialtySelectionScreen from "./screens/SpecialtySelectionScreen";
+import HomeScreen from "./screens/HomeScreen";
+import UserScreen from "./screens/UserScreen"; // Asegúrate de tener esta pantalla creada
 
 const Stack = createStackNavigator();
 
@@ -17,6 +19,8 @@ export default function App() {
         <Stack.Screen name="Register" component={RegisterScreen} />
         <Stack.Screen name="LanguageSelection" component={LanguageSelectionScreen} />
         <Stack.Screen name="SpecialtySelection" component={SpecialtySelectionScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="User" component={UserScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
